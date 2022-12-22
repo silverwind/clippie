99bc85b36850c5cd251e0e069087167b78099db0
silverwind <me@silverwind.io>
2022-12-22 17:02:47 +0100

    inline the fallback function

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ export async function clippie(content, {reject = false} = {}) {
         try {
           await navigator.clipboard.writeText(String(c));
         } catch {
-          return fallback(c);
+          if (!document.execCommand) return false;
+          const el = document.createElement("textarea");
+          el.value = c;
+          el.style.top = 0;
+          el.style.left = 0;
+          el.style.position = "fixed";
+          el.style.clip = "rect(0, 0, 0, 0)";
+          el.ariaHidden = "true";
+          document.body.appendChild(el);
+          el.select();
+          const success = document.execCommand("copy");
+          document.body.removeChild(el);
+          return success;
         }
       }
     }
@@ -17,20 +29,4 @@ export async function clippie(content, {reject = false} = {}) {
     if (reject) throw err;
     return false;
   }
-}
-
-function fallback(text) {
-  if (!document.execCommand) return false;
-  const el = document.createElement("textarea");
-  el.value = text;
-  el.style.top = 0;
-  el.style.left = 0;
-  el.style.position = "fixed";
-  el.style.clip = "rect(0, 0, 0, 0)";
-  el.ariaHidden = "true";
-  document.body.appendChild(el);
-  el.select();
-  const success = document.execCommand("copy");
-  document.body.removeChild(el);
-  return success;
 }
