473096287c2cec410b353877620fa53e08feae80
silverwind <me@silverwind.io>
2023-06-23 19:23:23 +0200

    update deps

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,9 @@
 import {defineConfig} from "vitest/dist/config.js";
+import {config} from "vitest-config-silverwind";
 
 export default defineConfig({
   test: {
+    ...config.test,
     environment: "jsdom",
-    testTimeout: 20000,
-    open: false,
-    allowOnly: true,
-    passWithNoTests: true,
-    globals: true,
-    watch: false,
-    outputDiffLines: Infinity,
-  },
+  }
 });
