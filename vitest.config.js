638d376798c8ee4b9a4929da5c5f19b93312ba46
silverwind <me@silverwind.io>
2023-06-30 01:07:51 +0200

    update deps

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,6 @@
 import {defineConfig} from "vitest/dist/config.js";
-import {config} from "vitest-config-silverwind";
+import {frontendTest} from "vitest-config-silverwind";
 
 export default defineConfig({
-  test: {
-    ...config.test,
-    environment: "jsdom",
-  }
+  test: frontendTest,
 });
