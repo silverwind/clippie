27b8b88b4491dae8582c4efacd42189be3acf471
silverwind <me@silverwind.io>
2024-03-26 02:18:05 +0100

    update deps, adjust vitest config

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,8 @@ import {defineConfig} from "vitest/config";
 import {frontendTest} from "vitest-config-silverwind";
 
 export default defineConfig({
-  test: {
-    ...frontendTest({url: import.meta.url}),
-  },
+  test: frontendTest({
+    url: import.meta.url,
+    environment: "jsdom", // happy-dom does not allow to modify navigator.clipboard
+  }),
 });
